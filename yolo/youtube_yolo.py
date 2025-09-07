f3495e140f82c48d4fc6ea6b630868794874e564
T.kawabata <52056772+momorara@users.noreply.github.com>
2025-09-07 15:42:53 +0900

    Add files via upload

--- yolo/youtube_yolo.py
+++ yolo/youtube_yolo.py
@@ -13,15 +13,50 @@ WebM (.webm)
 FLV (.flv)
 WMV (.wmv)
 
+2025/09/07  youtube動画をダウンロードして、yolo
+# yt-dlp をインストール
+pip install --upgrade yt-dlp
+
 q 終了
 s フレームをスキップ
 1,2,3 画像サイズ変更
 """
-from ultralytics import YOLO
+import os
+file_path = "video.mp4"
+# ファイルが存在するか確認して削除
+if os.path.exists(file_path):
+    os.remove(file_path)
+    print(f"{file_path} を削除しました。")
+else:
+    print(f"{file_path} は存在しません。")
+
+import yt_dlp
+
+# ダウンロードしたいYouTube動画のURL
+youtube_url = "https://www.youtube.com/watch?v="
+youtube_url = youtube_url + "Hnta4SrGIwo"
+
+# yt-dlp のオプション設定
+ydl_opts = {
+    'format': 'mp4',          # 動画フォーマットを mp4 に指定
+    'outtmpl': 'video.mp4',   # 保存するファイル名
+    'noplaylist': True,       # プレイリストではなく単一動画を取得
+    'progress_hooks': [],     # 進捗表示用（必要に応じて追加可能）
+}
+
+# yt-dlp の API を使って動画をダウンロード
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    try:
+        ydl.download([youtube_url])
+        print("ダウンロードが完了しました: video.mp4")
+    except Exception as e:
+        print("ダウンロード中にエラーが発生しました:", e)
+
+
 import cv2
-from picamera2 import Picamera2
 from imutils.video import FPS
 import time 
+from ultralytics import YOLO
 
 print()
 print("qキーの入力で終了します。")
@@ -37,7 +72,8 @@ model_name = model.ckpt_path # モデルファイルのパス
 print("yoloモデル:",model_name)  
 
 # 動画ファイルを開く
-video_path = "parson_640x360.mp4"
+video_path = "video.mp4"
+# video_path = "parson_640x360.mp4"
 #video_path = "Animals3.m4v"
 #video_path = "5_ume.mp4"
 #video_path = "5_maturi.mp4"
@@ -47,10 +83,12 @@ print("movie_file:",video_path)
 print()
 
 window_name = model_name + " Movie"
+
 # ウィンドウを作成（WINDOW_NORMALでリサイズ可能にする）
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
 # 任意の大きさにリサイズ
-cv2.resizeWindow(window_name, 640, 480)  # 幅640、高さ480
+cv2.resizeWindow(window_name, 640, 480)  # 幅1280、高さ720
 
 # FPS計測開始
 fps = FPS().start()
@@ -101,6 +139,7 @@ while cap.isOpened():
         mask = (confidences >= 0.6)
         person_count = mask.sum()
         #print("人間",person_count)  
+
     fps.update()
 
 # FPS計測終了
